69d818d1c9570757be608151b575294a8897abc6
saikia81 <saikia81@hotmail.com>
2016-02-25 05:31:59 +0100

    Initial commit

---
+++ GUI.py
@@ -0,0 +1,15 @@
+__author__ = 'Pablo'
+
+import kivy
+
+class Gui(object):
+    def __init__(self, courses):
+        self.courses = courses
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
